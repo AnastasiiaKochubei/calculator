70b570799e5ea5d342b9ba96defc6b4095735b6e
AnastasiiaKochubei <anastasiyaikochubey@gmail.com>
2019-06-06 22:07:20 +0300

    simple php function

--- calculator.php
+++ calculator.php
@@ -1,10 +1,52 @@
- Том в устройстве D имеет метку Новый том
- Серийный номер тома: C843-A8C7
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Calculator</title>
 
- Содержимое папки D:\openserver\OSPanel\domains\calculator
+    <title>Document</title>
+</head>
+<body>
 
-06.06.2019  20:22    <DIR>          .
-06.06.2019  20:22    <DIR>          ..
-06.06.2019  20:22                 0 calculator.php
-               1 файлов              0 байт
-               2 папок  300 814 491 648 байт свободно
+      <?php
+     //coment with two slash
+       echo "Calculator";
+       echo "<br>";
+      ?>
+
+      <?php
+      $a = 5;
+      $b = 6;
+      $c = 8;
+      $d = 12;
+      $summ =$a+$b;
+      $minus =$c - $d;
+      $devide = $d / $b;
+      $multiply =$b * $a;
+
+      echo $summ;
+      echo "<br>";
+      echo $minus;
+      echo "<br>";
+      echo $devide;
+      echo "<br>";
+      echo $multiply;
+      echo "<br>";
+      ?>
+      <?php
+      echo round(3.6, 0);
+      echo "<br>";
+      ?>
+      <?php
+      $a=66;
+      $procent=5;
+      $result = ($a*$procent)/100;
+      echo $result;
+      ?>
+
+
+</body>
+</html>
